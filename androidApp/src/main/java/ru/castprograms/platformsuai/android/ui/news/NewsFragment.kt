070b3473c85b7e9b920cb56bb984e850ba86ea47
UserNameMikko <mikkolitmanen10@gmail.com>
2022-07-15 16:47:28 +0300

    added news loading to a fragment

--- androidApp/src/main/java/ru/castprograms/platformsuai/android/ui/news/NewsFragment.kt
+++ androidApp/src/main/java/ru/castprograms/platformsuai/android/ui/news/NewsFragment.kt
@@ -3,14 +3,42 @@ package ru.castprograms.platformsuai.android.ui.news
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.castprograms.platformsuai.android.R
 import ru.castprograms.platformsuai.android.databinding.FragmentNewsBinding
+import ru.castprograms.platformsuai.android.ui.main.SmallNewsAdapter
+import ru.castprograms.platformsuai.util.Resource
+import ru.castprograms.platformsuai.viewModels.MainViewModel
 
 class NewsFragment: Fragment(R.layout.fragment_news) {
-
+    private val mainViewModel : MainViewModel by viewModel()
+    private lateinit var binding: FragmentNewsBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentNewsBinding.bind(view)
-        binding.recyclerNew.adapter // сюды адаптер
+        binding = FragmentNewsBinding.bind(view)
+        loadData()
+
     }
+    private fun loadData() {
+        loadBannersAndNews()
+    }
+    private fun loadBannersAndNews() {
+        MainScope().launch(Dispatchers.Main) {
+            mainViewModel.newsFlow.collectLatest {
+                if (it is Resource.Success) {
+                    requireActivity().runOnUiThread {
+                        binding.recyclerNew.adapter =
+                            SmallNewsAdapter(it.data?.Pubs?.items?.take(5) ?: listOf())
+                    }
+                } else {
+                    println(it.message)
+                }
+            }
+        }
+    }
+
 }

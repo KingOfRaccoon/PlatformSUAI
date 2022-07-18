f7b6e62e7aeb3805ca2ee2f35f23b76f780fb282
UserNameMikko <mikkolitmanen10@gmail.com>
2022-07-19 02:01:01 +0300

    added DiffUtil in NewsAdapter and some changes in NewsFragment

--- androidApp/src/main/java/ru/castprograms/platformsuai/android/ui/news/NewsAdapter.kt
+++ androidApp/src/main/java/ru/castprograms/platformsuai/android/ui/news/NewsAdapter.kt
@@ -1,8 +1,11 @@
 package ru.castprograms.platformsuai.android.ui.news
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import ru.castprograms.platformsuai.data.time.DataTime
 import ru.castprograms.platformsuai.android.R
@@ -10,9 +13,8 @@ import ru.castprograms.platformsuai.android.databinding.ItemNewBinding
 import ru.castprograms.platformsuai.android.ui.main.loadImage
 import ru.castprograms.platformsuai.data.news.Item
 
-class NewsAdapter(
-    private val news: List<Item>
-) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter/*( private val news: List<Item>)*/ :
+    RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         return NewsViewHolder(
             LayoutInflater.from(parent.context)
@@ -21,10 +23,12 @@ class NewsAdapter(
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        holder.bind(news[position])
+        //holder.bind(news[position])
+        holder.bind(differ.currentList[position])
+        Log.d("TAG", "bind position = $position")
     }
 
-    override fun getItemCount() = news.size
+    override fun getItemCount() = differ.currentList.size //news.size
 
     inner class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val binding = ItemNewBinding.bind(view)
@@ -35,4 +39,13 @@ class NewsAdapter(
             binding.textDayNew.text = DataTime(item.Date).getDate()
         }
     }
+
+    /* add diffUtil, now list is in differ.currentList */
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Item>() {
+        override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean =
+            oldItem.PubId == newItem.PubId
+        override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean = oldItem == newItem
+    }
+    val differ = AsyncListDiffer(this, diffCallback)
 }
--- androidApp/src/main/java/ru/castprograms/platformsuai/android/ui/news/NewsFragment.kt
+++ androidApp/src/main/java/ru/castprograms/platformsuai/android/ui/news/NewsFragment.kt
@@ -10,16 +10,17 @@ import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.castprograms.platformsuai.android.R
 import ru.castprograms.platformsuai.android.databinding.FragmentNewsBinding
-import ru.castprograms.platformsuai.android.ui.main.SmallNewsAdapter
 import ru.castprograms.platformsuai.util.Resource
 import ru.castprograms.platformsuai.viewModels.MainViewModel
 
 class NewsFragment : Fragment(R.layout.fragment_news) {
+    private val adapter = NewsAdapter()
     private val mainViewModel: MainViewModel by viewModel()
     private lateinit var binding: FragmentNewsBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNewsBinding.bind(view)
+        binding.recyclerNew.adapter = adapter
         loadData()
     }
 
@@ -32,8 +33,9 @@ class NewsFragment : Fragment(R.layout.fragment_news) {
             mainViewModel.newsFlow.collectLatest {
                 if (it is Resource.Success) {
                     requireActivity().runOnUiThread {
-                        binding.recyclerNew.adapter =
-                            NewsAdapter(it.data?.Pubs?.items?.take(9) ?: listOf())
+                        /*binding.recyclerNew.adapter =
+                            NewsAdapter(it.data?.Pubs?.items?.take(9) ?: listOf())*/
+                        adapter.differ.submitList(it.data?.Pubs?.items?.take(9)?:listOf())
                     }
                 } else {
                     println(it.message)
